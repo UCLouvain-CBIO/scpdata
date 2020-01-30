0437e81038a11e743343a726e9c36ad849199864
cvanderaa <vanderaa.christophe@gmail.com>
2020-01-30 17:24:17 +0100

    feat: added dou2019_mouse_peptide and dou2019_boosting_peptide data

--- inst/scripts/dou2019_boosting.R
+++ inst/scripts/dou2019_boosting.R
@@ -14,7 +14,7 @@
 library(openxlsx)
 library(MSnbase)
 library(SingleCellExperiment)
-# setwd("inst/scripts/")
+setwd("inst/scripts/")
 
 
 ####---- Experimental metadata ----####
@@ -38,10 +38,112 @@ expdat <- new("MIAPE",
               analyserDetails = "The MS1 scan was set at a mass  range  from  375  to  1575,  a  scan  resolution  of  120  k,  an  AGC  target  of  1E6,  and  a maximum  injection  time  of  50  ms.  Precursor  ions  with  charges  of  +2  to  +7  and intensities  >20,000  were  selected  for  MS/MS  sequencing.  Precursor  ions  were  isolated with an m/z window of 0.7 Th and fragmented by high energy dissociation (HCD) set at 35%. Repeat sampling were reduced with an exclusion duration of 60 s and m/z tolerance of ±10 ppm. MS/MS scan was carried out in the Orbitrap with an AGC target of 1E5. The MS/MS scan resolutions and maximum injection times were set as 60 k and 246 ms.",
               collisionEnergy = "HCD of 35%")
 
+## Sample annotation provided in Table S2 in the Supplementary information
+annot <- data.frame(Ion126 = c("Raw", "SVEC", "C10", "SVEC", "C10", "SVEC"),
+                    Ion127N = c("Raw", "SVEC", "C10", "SVEC", "C10", "SVEC"),
+                    Ion127C = c("Raw", "SVEC", "Raw", "SVEC", "C10", "SVEC"),
+                    Ion128N = c("SVEC", "C10", "Raw", "C10", "Raw", "C10"),
+                    Ion128C = c("SVEC", "C10", "Raw", "C10", "Raw", "C10"),
+                    Ion129N = c("SVEC", "C10", "SVEC", "C10", "Raw", "C10"),
+                    Ion129C = c("C10", "Raw", "SVEC", "Raw", "SVEC", "Raw"),
+                    Ion130N = c("C10", "Raw", "empty", "empty", "empty", "empty"),
+                    Ion130C = c("C10", "Raw", "C10", "Raw", "SVEC", "Raw"),
+                    Ion131N = c("empty", "empty", "boost", "boost", "boost", "boost"), 
+                    row.names = paste0("Boosting_", rep(c(0, 5, 50), each = 2), 
+                                       "ng_run_", rep(1:2, 3)),
+                    stringsAsFactors = FALSE)
+
 
 ####---- Peptide data ----####
 
-# TODO
+## Load and combine the identification and quantification data
+expNames <- list.files("../../extdata/dou2019/mzid/")
+expNames <- grep("^Boosting", expNames, value = TRUE)
+expNames <- sub("(Boosting.*)_msgf.*$", "\\1", expNames)
+## For every experiment (= MS run)
+dats <- lapply(expNames, function(exp){
+  ## Identification data
+  idFile <- list.files("../../extdata/dou2019/mzid/", pattern = exp, 
+                       full.names = TRUE)
+  mzidFile <- openIDfile(idFile)
+  mzid <- psms(mzidFile)
+  mzid <- mzid[!duplicated(mzid), ]
+  ## Remove PSMs that match decoy database
+  mzid <- mzid[!mzid$isDecoy, ]
+  ## Remove contaminants
+  mzid <- mzid[!grepl("Contaminant", mzid$DatabaseAccess), ]
+  
+  ## Quantification data
+  quantFile <- list.files("../../extdata/dou2019/quant/", pattern = exp, 
+                          full.names = TRUE)
+  quant <- read.table(quantFile, header = TRUE, sep = "\t")
+  
+  ## Combined data 
+  matchInds <- match(x = mzid$scan.number.s., 
+                     table = quant$ScanNumber, 
+                     nomatch = NA)
+  dat <- cbind(mzid, quant[matchInds, ])
+  
+  ## Create the MSnSet object
+  ecols <- grepl("^Ion_.*\\d$", colnames(dat))
+  ## TODO should scpdata also provide PSM data -> dou2019_mouse_psm ? 
+  dat <- psmDat <- MSnSet(exprs = as.matrix(dat[, ecols]), 
+                          fData = dat[, !ecols])
+  
+  ## Perform isotope correction
+  impurities <- makeImpuritiesMatrix(10, edit = FALSE) # TMT 10 was used
+  dat <- purityCorrect(dat, as.matrix(impurities))
+  
+  
+  ## Group the observation by peptide (modified and charged)
+  dat <- combineFeatures(object = dat, 
+                         groupBy = fData(dat)$sequence, 
+                         method = "sum", ## As done at protein level (see doc)
+                         cv = FALSE, 
+                         na.rm = TRUE)
+  
+  ## Filter only relevant feature data vars
+  keep <- c(peptide = "sequence", 
+            protein = "DatabaseAccess", 
+            proteinLength = "DBseqLength", 
+            proteinDescr = "DatabaseDescription",
+            startPos = "start", 
+            endPos = "end")
+  fData(dat) <- fData(dat)[, keep]
+  fvarLabels(dat) <- names(keep) ## Replace labels with more intuitive naming
+  
+  ## Extract the phenotype data
+  pData(dat) <- data.frame(Ion = sub("Ion_", "", colnames(dat)),
+                           ExperimentLabel = exp,
+                           SampleType = unlist(annot[exp, ]),
+                           Run = sub("^.*run_(\\d)$", "\\1", exp),
+                           Boosting = sub("^.*ing_(\\d*)ng.*$", "\\1", exp),
+                           row.names = colnames(dat))
+  dat <- updateSampleNames(dat, label = exp, sep = "_")
+  
+  ## Return data
+  cat("Formated:", exp, "\n")
+  return(dat)
+})
+
+## Merge the different runs
+dat <- do.call(combine, dats)
+## Note warnings are thrown because the levels in pData and fData don't match 
+## between runs. 
+## Replace NA's by 0's otherwise some SingleCellExperiment functions break
+## TODO this seems very dirty to me...
+exprs(dat)[is.na(exprs(dat))] <- 0 
+
+## Create SingleCellExperiment object
+dou2019_boosting_peptide <- 
+  SingleCellExperiment(assay = SimpleList(peptide = exprs(dat)),
+                       rowData = data.frame(fData(dat)), 
+                       colData = data.frame(pData(dat)),
+                       metadata = list(experimentData = expdat))
+## Save data as Rda file
+## Note: saving is assumed to occur in "scpdata/inst/scripts"
+save(dou2019_boosting_peptide, compress = "xz", compression_level = 9,
+     file = file.path("../../data/dou2019_boosting_peptide.rda"))
 
 
 ####---- Protein data ----####
--- inst/scripts/dou2019_mouse.R
+++ inst/scripts/dou2019_mouse.R
@@ -14,7 +14,7 @@
 library(openxlsx)
 library(MSnbase)
 library(SingleCellExperiment)
-# setwd("inst/scripts/")
+setwd("inst/scripts/")
 
 
 ####---- Experimental data ----####
@@ -38,10 +38,117 @@ expdat <- new("MIAPE",
               analyserDetails = "The MS1 scan was set at a mass  range  from  375  to  1575,  a  scan  resolution  of  120  k,  an  AGC  target  of  1E6,  and  a maximum  injection  time  of  50  ms.  Precursor  ions  with  charges  of  +2  to  +7  and intensities  >20,000  were  selected  for  MS/MS  sequencing.  Precursor  ions  were  isolated with an m/z window of 0.7 Th and fragmented by high energy dissociation (HCD) set at 35%. Repeat sampling were reduced with an exclusion duration of 60 s and m/z tolerance of ±10 ppm. MS/MS scan was carried out in the Orbitrap with an AGC target of 1E5. The MS/MS scan resolutions and maximum injection times were set as 60 k and 246 ms.",
               collisionEnergy = "HCD of 35%")
 
+## Sample annotation provided in Table S3 in the Supplementary information
+annot <- data.frame(Ion126 = c(rep("C10", 6), rep("SVEC", 6)),
+                    Ion127N = c(rep("C10", 3), rep("SVEC", 6), rep("RAW", 3)),
+                    Ion127C = c(rep("SVEC", 6), rep("RAW", 6)),
+                    Ion128N = c(rep("SVEC", 3), rep("RAW", 6), rep("C10", 3)),
+                    Ion128C = c(rep("RAW", 6), rep("C10", 6)),
+                    Ion129N = c(rep("RAW", 3), rep("C10", 6), rep("SVEC", 3)),
+                    Ion129C = c(rep("reference", 12)),
+                    Ion130N = c(rep("empty", 12)),
+                    Ion130C = c(rep("empty", 12)),
+                    Ion131N = c(rep("boost", 12)), 
+                    ## Note there is a mistake in the table. The author provide 
+                    ## the annotation for TMT-10 whereas TMT-11 was used. The 
+                    ## last column in the data has little signal, so sample 
+                    ## type is assumed to be "empty"
+                    Ion131C = c(rep("empty", 12)), 
+                    row.names = paste0("Chip_", rep(1:4, each = 3), "_",
+                                       rep(c("A", "B", "C"), 4)),
+                    stringsAsFactors = FALSE)
 
-####---- Protein data ----####
 
-# TODO
+####---- Peptide data ----####
+
+## Load and combine the identification and quantification data
+expNames <- list.files("../../extdata/dou2019/mzid/")
+expNames <- grep("^Single_Cell", expNames, value = TRUE)
+expNames <- sub("(Single.*)_msgf.*$", "\\1", expNames)
+## For every experiment (= MS run)
+dats <- lapply(expNames, function(exp){
+  ## Identification data
+  idFile <- list.files("../../extdata/dou2019/mzid/", pattern = exp, 
+                       full.names = TRUE)
+  mzidFile <- openIDfile(idFile)
+  mzid <- psms(mzidFile)
+  mzid <- mzid[!duplicated(mzid), ]
+  ## Remove PSMs that match decoy database
+  mzid <- mzid[!mzid$isDecoy, ]
+  ## Remove contaminants
+  mzid <- mzid[!grepl("Contaminant", mzid$DatabaseAccess), ]
+  
+  ## Quantification data
+  quantFile <- list.files("../../extdata/dou2019/quant/", pattern = exp, 
+                          full.names = TRUE)
+  quant <- read.table(quantFile, header = TRUE, sep = "\t")
+  
+  ## Combined data 
+  matchInds <- match(x = mzid$scan.number.s., 
+                     table = quant$ScanNumber, 
+                     nomatch = NA)
+  dat <- cbind(mzid, quant[matchInds, ])
+  
+  ## Create the MSnSet object
+  ecols <- grepl("^Ion_.*\\d$", colnames(dat))
+  ## TODO should scpdata also provide PSM data -> dou2019_mouse_psm ? 
+  dat <- psmDat <- MSnSet(exprs = as.matrix(dat[, ecols]), 
+                          fData = dat[, !ecols])
+  
+  ## Perform isotope correction
+  impurities <- read.csv("../../extdata/specht2019/te269088_lot_correction.csv",
+                         check.names = FALSE, row.names = 1)
+  dat <- purityCorrect(dat, as.matrix(impurities))
+  
+  ## Group the observation by peptide (modified and charged)
+  dat <- combineFeatures(object = dat, 
+                         groupBy = fData(dat)$sequence, 
+                         method = "sum", ## As done at protein level (see doc)
+                         cv = FALSE, 
+                         na.rm = TRUE)
+  
+  ## Filter only relevant feature data vars
+  keep <- c(peptide = "sequence", 
+            protein = "DatabaseAccess", 
+            proteinLength = "DBseqLength", 
+            proteinDescr = "DatabaseDescription",
+            startPos = "start", 
+            endPos = "end")
+  fData(dat) <- fData(dat)[, keep]
+  fvarLabels(dat) <- names(keep) ## Replace labels with more intuitive naming
+  
+  ## Extract the phenotype data
+  chip <- sub("^S.*(Chip_\\d_[A-C])$", "\\1", exp)
+  pData(dat) <- data.frame(Ion = gsub("Ion_", "", colnames(dat)),
+                           ExperimentLabel = exp,
+                           Chip = chip,
+                           SampleType = unlist(annot[chip, ]),
+                           row.names = colnames(dat))
+  dat <- updateSampleNames(dat, label = exp, sep = "_")
+  
+  ## Return data
+  cat("Formated:", exp, "\n")
+  return(dat)
+})
+
+## Merge the different runs
+dat <- do.call(combine, dats)
+## Note warnings are thrown because the levels in pData and fData don't match 
+## between runs. 
+## Replace NA's by 0's otherwise some SingleCellExperiment functions break
+## TODO this seems very dirty to me...
+exprs(dat)[is.na(exprs(dat))] <- 0 
+
+## Create SingleCellExperiment object
+dou2019_mouse_peptide <- 
+  SingleCellExperiment(assay = SimpleList(peptide = exprs(dat)),
+                       rowData = data.frame(fData(dat)), 
+                       colData = data.frame(pData(dat)),
+                       metadata = list(experimentData = expdat))
+## Save data as Rda file
+## Note: saving is assumed to occur in "scpdata/inst/scripts"
+save(dou2019_mouse_peptide, compress = "xz", compression_level = 9,
+     file = file.path("../../data/dou2019_mouse_peptide.rda"))
 
 
 ####---- Protein data ----####
