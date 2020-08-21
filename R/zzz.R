6ee0a7bd495c1dee85737b72b84713598b9d45dc
Christophe Vanderaa <vanderaa.christophe@gmail.com>
2020-08-21 11:39:19 +0200

    feat: added .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,15 @@
-# Put onload and onAttach functionality
+## Put onload and onAttach functionality
 
 .onAttach <- function(libname, pkgname) {
   msg <- paste0("\nThis is scpdata version ",
                 packageVersion("scpdata"), ".\n",
                 "Use 'scpdata()' to list available data sets.")
   packageStartupMessage(msg)  
+}
+
+.onLoad <- function(libname, pkgname) {
+  ## exports each resource name (i.e., title) into a function
+  fl <- system.file("extdata", "metadata.csv", package=pkgname)
+  titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
+  createHubAccessors(pkgname, titles)
 }
