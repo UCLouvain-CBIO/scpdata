5545b50324a1b8c3c736e047c3512784102075d2
Christophe Vanderaa <vanderaa.christophe@gmail.com>
2020-08-21 12:25:51 +0200

    doc: added package man page

---
+++ R/package_doc.R
@@ -0,0 +1,35 @@
+
+####---- SCP PACKAGE MAN PAGE ----####
+
+
+##' Single Cell Proteomics Data Package
+##' 
+##' Data package distributing mass spectrometry-based single-cell proteomics 
+##' datasets from published work. The datasets are curated and formated to 
+##' a standardized data framework. This frameworks stores the different assays
+##' (different batches for PSM data, peptide data and protein data) in 
+##' `SingleCellExperiment` objects. The assays are stored in a single 
+##' `QFeatures` object. 
+##' 
+##' @seealso More information about the data framework can be found in the 
+##' [scp] package
+##' 
+##' 
+##' @examples 
+##' library(ExperimentHub)
+##' 
+##' ## Load data using the ExperimentHub interface
+##' hub <- ExperimentHub()
+##' ## x <- query(hub, c("scpdata", "specht2019v2"))
+##' 
+##' \dontrun{
+##' ## download resource
+##' dataset <- x[[1]]
+##' }
+##'
+##' ## Load data by name
+##' x <- specht2019v2(metadata = FALSE)
+##' 
+##' 
+##' @aliases scpdata-package scpdata
+"scp-package"
