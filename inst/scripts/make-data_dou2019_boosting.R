c0820cd9501d94a406582fb17f83dd24e591b56d
Christophe Vanderaa <vanderaa.christophe@gmail.com>
2021-10-14 15:54:08 +0200

    dou2019_boosting: started fixing data

--- inst/scripts/make-data_dou2019_boosting.R
+++ inst/scripts/make-data_dou2019_boosting.R
@@ -15,52 +15,53 @@ library(openxlsx)
 library(scp)
 library(mzR)
 library(tidyverse)
-setwd("inst/scripts/")
-dataDir <- "../extdata/dou2019_boosting/"
+dataDir <- "~/PhD/.localdata/SCP/dou2019_boosting/"
 
 ####---- PSM data ----####
 
-
 ## Load and combine the identification and quantification data
 list.files(dataDir) %>%
-  sub(pattern = "_[a-z]*[.][a-z]*$", 
-      replacement = "", ignore.case = TRUE) %>%
-  unique %>%
-  grep(pattern = "xlsx", invert = TRUE, value = TRUE)->
-  batches
+    sub(pattern = "_[a-z]*[.][a-z]*$", 
+        replacement = "", ignore.case = TRUE) %>%
+    unique %>%
+    grep(pattern = "xlsx", invert = TRUE, value = TRUE)->
+    batches
 ## For every experiment (= MS run)
 lapply(batches, function(batch){
-  ## Identification data (.mzid files) were downloaded at 
-  ## ftp://massive.ucsd.edu/MSV000084110/result/Result_Files/
-  list.files(path = dataDir, 
-             pattern = paste0(batch, ".*mzid$"), 
-             full.names = TRUE) %>%
-    ## Read in files using `mzR`
-    openIDfile %>%
-    psms %>%
-    ## Rename column to match with quantification data
-    dplyr::rename(ScanNumber = scan.number.s.,
-                  ## Avoid forbidden names for SingleCellExperiment class
-                  .start = start,
-                  .end = end) %>% 
-    ## Remove duplicate rows
-    distinct -> 
-    mzid
-  ## Quantification data (.txt files) were downloaded at
-  ## ftp://massive.ucsd.edu/MSV000084110/other/MASIC_ReporterIons/
-  list.files(path = dataDir, 
-             pattern = paste0(batch, ".*txt$"), 
-             full.names = TRUE) %>%
-    read.table(header = TRUE, sep = "\t") %>%
-    ## Add the batch name 
-    mutate(Batch = batch) ->
-    quant
-  ## Combine data 
-  left_join(mzid, quant, by = "ScanNumber")
+    ## Identification data (.mzid files) were downloaded at 
+    ## ftp://massive.ucsd.edu/MSV000084110/result/Result_Files/
+    list.files(path = dataDir, 
+               pattern = paste0(batch, ".*mzid$"), 
+               full.names = TRUE) %>%
+        ## Read in files using `mzR`
+        openIDfile %>%
+        psms %>%
+        ## Rename column to match with quantification data
+        dplyr::rename(ScanNumber = scan.number.s.,
+                      ## Avoid forbidden names for SingleCellExperiment class
+                      .start = start,
+                      .end = end) %>% 
+        ## Remove duplicate rows
+        distinct -> 
+        mzid
+    ## Quantification data (.txt files) were downloaded at
+    ## ftp://massive.ucsd.edu/MSV000084110/other/MASIC_ReporterIons/
+    list.files(path = dataDir, 
+               pattern = paste0(batch, ".*txt$"), 
+               full.names = TRUE) %>%
+        read.table(header = TRUE, sep = "\t") %>%
+        ## Add the batch name 
+        mutate(Batch = batch) ->
+        quant
+    ## Combine data 
+    left_join(mzid, quant, by = "ScanNumber")
 }) %>%
-  ## Bind the different batches
-  bind_rows ->
-  dat
+    ## Bind the different batches
+    bind_rows ->
+    dat
+
+## PSM is mapped to multiple isoforms, we add the isoform subscript to
+## the peptide sequence
 
 ## Sample annotation provided in Table S3 in the Supplementary information
 data.frame(Batch = batches,
@@ -75,10 +76,10 @@ data.frame(Batch = batches,
            Ion_130.135 = c("C10", "Raw", "empty", "empty", "empty", "empty"),
            Ion_130.141 = c("C10", "Raw", "C10", "Raw", "SVEC", "Raw"),
            Ion_131.138 = c("empty", "empty", "boost", "boost", "boost", "boost")) %>%
-  pivot_longer(cols = matches("Ion"), 
-               names_to = "Channel", 
-               values_to = "SampleType") ->
-  colDat
+    pivot_longer(cols = matches("Ion"), 
+                 names_to = "Channel", 
+                 values_to = "SampleType") ->
+    colDat
 
 ## Create the `QFeatures` object
 dou2019_boosting <- readSCP(dat, 
@@ -86,55 +87,59 @@ dou2019_boosting <- readSCP(dat,
                             batchCol = "Batch", 
                             channelCol = "Channel")
 
+####---- Peptide data ----####
+
+## We generate the peptide data ourself, through median aggregation
+dou2019_boosting <- 
+    aggregateFeaturesOverAssays(dou2019_boosting, 
+                                i = seq_along(dou2019_boosting),
+                                fcol = "sequence", 
+                                name = paste0("pep_", names(dou2019_boosting)),
+                                fun = colMedians)
+## We join the peptide data in a single assay
+dou2019_boosting <- joinAssays(dou2019_boosting, 
+                               i = grep("^pep_", names(dou2019_boosting)), 
+                               name = "peptides")
+## We remove the intermediate aggregation assays
+dou2019_boosting <- dou2019_boosting[, , !grepl("^pep_", names(dou2019_boosting))]
 
 ####---- Protein data ----####
 
-
 ## Load the data
 ## Data was downloaded from https://doi.org/10.1021/acs.analchem.9b03349.
-list.files(path = dataDir, 
-           pattern = "xlsx$",  
-           full.names = TRUE) %>%
-  loadWorkbook ->
-  datXlsx
-
+datXlsx <- loadWorkbook(list.files(path = dataDir, 
+                                   pattern = "xlsx$",  
+                                   full.names = TRUE))
 sheets <- c(2, ## 0ng boosting
             4, ## 5ng boosting
             6) ## 50ng boosting
 lapply(sheets, 
        read.xlsx,
        xlsxFile = datXlsx, colNames = TRUE, startRow = 2) %>%
-  ## Remove the peptide and spectra counts
-  lapply(function(x) x[, -(2:3)]) %>%
-  ## Joint the datasets based on the protein
-  reduce(full_join, by = "Protein") ->
-  dat
+    ## Remove the peptide and spectra counts
+    lapply(function(x) x[, -(2:3)]) %>%
+    ## Joint the datasets based on the protein
+    reduce(full_join, by = "Protein") ->
+    prots
 
 ## Rename columns
-colnames(dat) %>%
-  data.frame %>%
-  mutate(Channel = sub(pattern = "_Data.*$", replacement = "", .),
-         DatasetID = sub(pattern = "Ion.*ID_", replacement = "",  .),
-         Batch = colDat$Batch[match(DatasetID, colDat$DatasetID)],
-         colname = ifelse(grepl("Ion", Channel),
-                          paste0(Batch, "_", Channel),
-                          Channel)) %>%
-  pull(colname) ->
-  colname
+channel <- sub(pattern = "_Data.*$", replacement = "", colnames(prots))
+datasetID <- sub(pattern = "Ion.*ID_", replacement = "",  colnames(prots))
+batch <- colDat$Batch[match(datasetID, colDat$DatasetID)]
+colnames(prots) <- ifelse(grepl("Ion", channel), paste0(batch, "_", channel),
+                          channel)
 
 ## Extract and format the protein expression data 
-dat %>%
-  magrittr::set_colnames(colname) %>%
-  readSingleCellExperiment(ecol = grep("Ion", colname),
-                           fnames = "Protein") ->
-  dat
+prots <- readSingleCellExperiment(prots, 
+                                  ecol = grep("Ion", colname),
+                                  fnames = "Protein")
 
 ## Add assay and AssayLinks to the dataset
-addAssay(dou2019_boosting, dat, name = "proteins") %>%
-  addAssayLink(from = 1:6, to = "proteins", 
-               varFrom = rep("DatabaseAccess", 6), 
-               varTo = "Protein") ->
-  dou2019_boosting
+addAssay(dou2019_boosting, prots, name = "proteins") %>%
+    addAssayLink(from = "peptides", to = "proteins", 
+                 varFrom = "DatabaseAccess", 
+                 varTo = "Protein") ->
+    dou2019_boosting
 
 ## Save data as Rda file
 ## Note: saving is assumed to occur in "scpdata/inst/scripts"
