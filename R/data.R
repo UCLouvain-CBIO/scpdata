08b91ec5f9bfabaf749cf3c09185ca034afb2efe
Laurent Gatto <laurent.gatto@uclouvain.be>
2024-03-11 13:33:08 +0100

    spell out beta

--- R/data.R
+++ R/data.R
@@ -2372,35 +2372,36 @@
 ##'
 ##' @description
 ##'
-##' Single-cell samples were prepared using the nPOP sample preparation method. 
-##' Proteomics data were acquired using the SCoPE2 protocol on a Thermo 
-##' Scientific Q-Exactive mass spectrometer. The dataset contains quantitative 
-##' information on 421 MCF-10A single cells undergoing epithelial–mesenchymal 
-##' transition (EMT) triggered by TGFβ. The data are available at the PSM, and 
-##' protein levels. The paper investigates the dynamics of correlation modules
-##' at the protein level.
+##' Single-cell samples were prepared using the nPOP sample
+##' preparation method.  Proteomics data were acquired using the
+##' SCoPE2 protocol on a Thermo Scientific Q-Exactive mass
+##' spectrometer. The dataset contains quantitative information on 421
+##' MCF-10A single cells undergoing epithelial–mesenchymal transition
+##' (EMT) triggered by TGF beta. The data are available at the PSM,
+##' and protein levels. The paper investigates the dynamics of
+##' correlation modules at the protein level.
 ##'
 ##' @format A [QFeatures] object with 47 assays, each assay being a
 ##' [SingleCellExperiment] object:
 ##'
 ##' - Assay 1-44: PSM data acquired with a TMTPro 16plex protocol, hence
 ##'   those assays contain 16 columns. Columns hold quantitative information
-##'   from single-cell channels, carrier channels, reference channels, 
+##'   from single-cell channels, carrier channels, reference channels,
 ##'   empty (negative control) channels and unused channels.
 ##' - `peptides`: peptide data containing quantitative data for 10055
-##'   peptides and 421 single-cells. 
+##'   peptides and 421 single-cells.
 ##' - `proteins_imputed`: protein data containing quantitative data for 4096
-##'   proteins and 421 single-cells with k-nearest neighbors (KNN) imputation. 
+##'   proteins and 421 single-cells with k-nearest neighbors (KNN) imputation.
 ##' - `proteins_unimputed`: protein data containing quantitative data for 4096
 ##'   proteins and 421 single-cells without imputation.
-##'   
-##' The `colData(khan2023())` contains cell type and batch annotations that 
-##' are common to all assays. The description of the `rowData` fields for the 
+##'
+##' The `colData(khan2023())` contains cell type and batch annotations that
+##' are common to all assays. The description of the `rowData` fields for the
 ##' PSM data can be found in the
 ##' [`MaxQuant` documentation](https://cox-labs.github.io/coxdocs/output_tables.html).
 ##'
 ##' @section Acquisition protocol:
-##' 
+##'
 ##' The data were acquired using the following setup. More information
 ##' can be found in the source article (see `References`).
 ##'
@@ -2431,29 +2432,29 @@
 ##' columns match with those of the annotation and filter only for
 ##' single-cell runs. Both table are then combined in a single
 ##' [QFeatures] object using the [scp::readSCP] function.
-##' 
+##'
 ##' The peptide data were taken from the same google drive folder
 ##' (`EpiToMesen.TGFB.nPoP_trial1_pepByCellMatrix_NSThreshDART_medIntCrNorm.txt`).
 ##' The data were formated to a [SingleCellExperiment] object and the sample
 ##' metadata were matched to the column names (mapping is retrieved
 ##' after running the SCoPE2 R script, `EMTTGFB_singleCellProcessing.R`) and
-##' stored in the `colData`. The object is then added to the [QFeatures] object 
+##' stored in the `colData`. The object is then added to the [QFeatures] object
 ##' and the rows of the PSM data are linked to the rows of the peptide data
 ##' based on the peptide sequence information through an `AssayLink` object.
-##' 
+##'
 ##' The imputed protein data were taken from the same google drive folder
 ##' (`EpiToMesen.TGFB.nPoP_trial1_ProtByCellMatrix_NSThreshDART_medIntCrNorm_imputedNotBC.csv`).
 ##' The data were formated to a [SingleCellExperiment] object and the sample
 ##' metadata were matched to the column names (mapping is retrieved
 ##' after running the SCoPE2 R script, `EMTTGFB_singleCellProcessing.R`) and
-##' stored in the `colData`. The object is then added to the [QFeatures] object 
+##' stored in the `colData`. The object is then added to the [QFeatures] object
 ##' and the rows of the peptide data are linked to the rows of the protein data
 ##' based on the protein sequence information through an `AssayLink` object.
 ##'
 ##' The unimputed protein data were taken from the same google drive folder
 ##' (`EpiToMesen.TGFB.nPoP_trial1_ProtByCellMatrix_NSThreshDART_medIntCrNorm_unimputed.csv`).
 ##' The data were formated and added exactly as imputed data.
-##' 
+##'
 ##' @source
 ##' The data were downloaded from the
 ##' [Slavov Lab](https://scp.slavovlab.net/Khan_et_al_2023) website via a
@@ -2463,7 +2464,7 @@
 ##' MassIVE repository `MSV000092872`:
 ##' ftp://MSV000092872@massive.ucsd.edu/.
 ##'
-##' @references 
+##' @references
 ##' Saad Khan, Rachel Conover, Anand R. Asthagiri, Nikolai Slavov. 2023.
 ##' "Dynamics of single-cell protein covariation during epithelial–mesenchymal
 ##' transition." bioRxiv.
@@ -2477,4 +2478,3 @@
 ##' @keywords datasets
 ##'
 "khan2023"
-
