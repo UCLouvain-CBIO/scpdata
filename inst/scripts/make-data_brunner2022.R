933e276652ecac2558dad75a3fb214e2f94ed4e8
Laurent Gatto <lgatto@protonmail.ch>
2026-09-06 09:55:24 +0200

    full/new brunner2022 script

--- inst/scripts/make-data_brunner2022.R
+++ inst/scripts/make-data_brunner2022.R
@@ -1,10 +1,14 @@
-library("SCP.replication")
 library("tidyverse")
 library("data.table")
+library("QFeatures")
+library("scp")
 
-## report <- fread("~/PhD/.localdata/SCP/brunner2022/20210919_DIANN_SingleCellOutput.tsv")
-report <- fread("~/Downloads/DIANN1.8_SingleCells_CellCycle/20210919_DIANN_SingleCellOutput.tsv")
-report$File.Name <- sub("^.+2020", "2020", report$File.Name)
+## The data files used below are available in
+## DIANN1.8_SingleCells_CellCycle[.7z|zip] from PRIDE at
+## https://www.ebi.ac.uk/pride/archive/projects/PXD024043?
+
+report <- fread("~/tmp/DIANN1.8_SingleCells_CellCycle/20210919_DIANN_SingleCellOutput.tsv")
+report$File.Name <- sub("^.+2020", "2020", report$File.Name) ## clean up filenames
 
 ## Build the annotation table from the run names
 annot <- DataFrame(File.Name = unique(report$File.Name))
@@ -25,55 +29,69 @@ annot <- cbind(annot, otherVars)
 ## Format to a QFeatures object
 ## brunner2022 <- readSCPfromDIANN(annot, report)
 brunner2022 <- readSCPfromDIANN(assayData = report, colData = annot)
-names(brunner2022) <- sub("^.+2020", "2020", names(brunner2022))
+## change set names
+names(brunner2022) <- sub("\\.d", "", sub("2020.+_", "prec_", names(brunner2022)))
 
-brunner2022 <- aggregateFeatures(
-    brunner2022,
-    i = seq_along(brunner2022),
-    fcol = "Modified.Sequence",
-    name = paste0("modpep_",
-                  sub("\\.d", "", sub("^.+_", "", names(brunner2022)))),
-    fun = colMedians,
-    na.rm = TRUE)
+## Aggregate precursor into modified peptides
+brunner2022 <-
+    aggregateFeatures(brunner2022,
+                      i = seq_along(brunner2022),
+                      fcol = "Modified.Sequence",
+                      name = sub("prec", "modpep", names(brunner2022)),
+                      fun = colMedians,
+                      na.rm = TRUE)
 
-brunner2022 <- joinAssays(brunner2022,
-                          grep("modpep", names(brunner2022)),
-                          name = "modpep")
-brunner2022 <- logTransform(brunner2022, i = "modpep",
-                            name = "logmodpep")
-brunner2022 <- aggregateFeatures(brunner2022,
-                                 i = "logmodpep",
-                                 name = "logpep",
-                                 fcol = "Stripped.Sequence",
-                                 fun = colMedians)
-brunner2022 <- aggregateFeatures(brunner2022,
-                                 i = "logpep",
-                                 name = "logprot",
-                                 fcol = "Protein.Group",
-                                 fun = colMedians)
+## Join modififed peptides into a wide table
+brunner2022 <-
+    joinAssays(brunner2022,
+               grep("modpep", names(brunner2022)),
+               name = "modpep")
 
+## Log-transforme the modified peptide table
+brunner2022 <-
+    logTransform(brunner2022,
+                 i = "modpep",
+                 name = "logmodpep")
 
-## The protein data
-## pgTable <- read.delim("~/PhD/.localdata/SCP/brunner2022/20210919_DIANN_SingleCellOutput.pg_matrix.tsv",
-##                       check.names = FALSE)
+## Aggregate (log-transformed) modified peptides into stripped peptide sequences
+brunner2022 <-
+    aggregateFeatures(brunner2022,
+                      i = "logmodpep",
+                      name = "logpep",
+                      fcol = "Stripped.Sequence",
+                      fun = colMedians)
+## Aggregate stripped peptides into (log-transformed) proteins
+brunner2022 <-
+    aggregateFeatures(brunner2022,
+                      i = "logpep",
+                      name = "logproteins",
+                      fcol = "Protein.Group",
+                      fun = colMedians)
 
-pgTable <- read.delim("~/Downloads/DIANN1.8_SingleCells_CellCycle/20210919_DIANN_SingleCellOutput.pg_matrix.tsv",
+## Add the protein data provided by the authors, and link it to the precursors
+pgTable <- read.delim("~/tmp/DIANN1.8_SingleCells_CellCycle/20210919_DIANN_SingleCellOutput.pg_matrix.tsv",
                       check.names = FALSE)
 names(pgTable) <- sub("^.+2020", "2020", names(pgTable))
-prots <- readSingleCellExperiment(pgTable, quantCols = unique(report$File.Name),
-                                  fnames = "Protein.Names")
+prots <-
+    readSingleCellExperiment(pgTable,
+                             quantCols = unique(report$File.Name),
+                             fnames = "Protein.Names")
+
 prots <- prots[rownames(prots) != ""]
 rownames(prots) <- make.unique(rownames(prots))
-from <- names(brunner2022)
-brunner2022 <- addAssay(brunner2022, prots, "proteins")
-brunner2022 <- addAssayLink(brunner2022,
-                            from = from,
-                            to = "proteins",
-                            varFrom = rep("Protein.Group", length(from)),
-                            varTo = "Protein.Group")
+from <- grep("prec", names(brunner2022), value = TRUE)
+
+brunner2022 <- addAssay(brunner2022, prots, "proteinsDiann")
+
+brunner2022 <-
+    addAssayLink(brunner2022,
+                 from = from,
+                 to = "proteinsDiann",
+                 varFrom = rep("Protein.Group", length(from)),
+                 varTo = "Protein.Group")
 
 # Save data as Rda file
 save(brunner2022,
-     file = "~/PhD/.localdata/scpdata/brunner2022.Rda",
+     file = "~/tmp/DIANN1.8_SingleCells_CellCycle/brunner2022.Rda",
      compress = "xz",
      compression_level = 9)
